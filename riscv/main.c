5e65287cb6f25597d5f44bb24ca7d8e6d091fc58
Martin <mart256@gmail.com>
2026-01-02 19:29:46 +0100

    Using newer libc

--- riscv/main.c
+++ riscv/main.c
@@ -2,6 +2,9 @@
 #include "utils.h"
 #include "spi.h"
 
+static unsigned long multix(unsigned long a, unsigned long b) {
+    return (a + 5) * b;
+}  
 
 int main() {
     cls();
@@ -9,6 +12,10 @@ int main() {
     unsigned char* content = read_flash();
 
     println(content);
+
+    unsigned char temp[11];
+    ultoa(multix(10, 15), temp);
+    println(temp);
 
     println("Starting up TinyBasic Plus from RiscV...");
 
