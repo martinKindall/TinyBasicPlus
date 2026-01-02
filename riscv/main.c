0a226b3e76a16da1540206f3d08dfdea33f7e670
Martin <mart256@gmail.com>
2026-01-02 19:51:35 +0100

    Using GCC for loading. libgcc gets loaded automatically

--- riscv/main.c
+++ riscv/main.c
@@ -1,21 +1,11 @@
 #include "desktop.h"
-#include "utils.h"
 #include "spi.h"
-
-static unsigned long multix(unsigned long a, unsigned long b) {
-    return (a + 5) * b;
-}  
 
 int main() {
     cls();
     
     unsigned char* content = read_flash();
-
     println(content);
-
-    unsigned char temp[11];
-    ultoa(multix(10, 15), temp);
-    println(temp);
 
     println("Starting up TinyBasic Plus from RiscV...");
 
